31572cab74525c641f971daa9ede556aceb3ec7f
Ermand Haruni <eharuni@k1m09s05.42istanbul.com.tr>
2022-06-07 16:16:06 +0300

    first commit

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eharuni <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 20:40:37 by eharuni           #+#    #+#             */
+/*   Updated: 2021/12/10 20:41:06 by eharuni          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]) && str[i])
+	{
+		res = res * 10 + str[i] - 48;
+		i++;
+	}
+	return (res * sign);
+}
